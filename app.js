1cdb849fb62d112a6614a9bc7f81f7e1ee732d17
AndrewHatch <03110h@gmail.com>
2021-11-02 14:51:15 -0400

    test

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+const socket = require('socket.io');
+const PORT = process.env.PORT || 3000;
+const cookieSession = require('cookie-sessions');
+// const { find } = require('async');
+const passport = require('passport');
+require('./auth/passport-config')(passport);
+
+
+app.set('view engine','ejs')
+app.use(express.static('public'));
+app.use(helmet())
+app.use(cookieSession({
+    name:'session',
+    keys:['CaptainJacobKeyes'],
+    maxAge: 14*24*60*60*1000
+}))
+
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+
+app.use(require('./routes/index'))
+app.use(require('./routes/homepage'))
+app.use(require('./routes/userpage'))
+app.use(require('./routes/about'))
+app.use(require('./routes/chat'))
+app.use(require('./routes/404'))
+app.use(require('./routes/login'))
+
+
+
+
+let server = app.listen(PORT,() => {
+    console.log(`Listening on ${PORT}`);
+    
+})  
+
+let io = socket(server)
+
+
+io.on('connection', (socket) => {
+    console.log('client connected');
+    socket.on('postMessage', (clientMsg) => {    //listens for incoming chat messages
+
+        io.emit('updateMessage',clientMsg) //broadcasts back out to all the clients
+        
+    })
+})
+
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,14 @@
+
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const passport = require('passport');
 
 router.get('/login', (req,res) => {
-
-
     res.render('login')
-    
 })
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}))
+
+module.exports = router;
