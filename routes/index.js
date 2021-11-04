dee08be4974ef1110ac4267ee0bb3fa420502e88
Victoria Walker <vmsmith2014@gmail.com>
2021-11-04 12:58:16 -0400

    manipulating data into arrays; need to finish dates and cleanup

--- routes/index.js
+++ routes/index.js
@@ -17,38 +17,84 @@ router.get('/', async (req,res) => {
             }
         }
     });
-    
-    res.render('index', {
-        recentPosts: recentPosts
-    });
-});
 
-router.get('/blogs', async (req,res) => {
-    //put gatekeeper to get user id
-    let date = new Date()
-    date.setDate(date.getDate() - 3)
-    console.log(date);
-
-    // let recentPosts = await db.posts.findAll(
-    //     {
+    // let recentPostswithUsers = await db.posts.findAll({
     //     where: {
     //         createdAt: {
     //             [Op.gte]: date
     //         }
     //     },
-    //      include: {}
-    // }
-    // );
-    let posts = await db.posts.findAll({include: [{
-        model: db.users,
-        required: true
-    }]})
+    //     include: [{
+    //         model: db.users,
+    //         required: true
+    //     }]
+    // });
+    
+    // console.log(recentPosts);
+    // data structure: [posts {dataValues: {id: num, title: 'string', ... user: []}, _previousDataValues: {}, ...}]
+    // console.log(recentPostswithUsers[0].dataValues.user);
+    // let recentPosts = recentPostswithUsers[0].dataValues
+    // let users = recentPostswithUsers[0].dataValues.user.dataValues
+    console.log(recentPosts);
+    console.log("---------");
+    console.log(users);
 
-    let userRecord = await db.users.findByPk(1) //req.user.id 
-    console.log(userRecord);
-    // let followingUsers = userRecord.following
-    res.json(posts)
-})
+    let dates = []
+    let users = []
+
+    // let testDate = recentPostswithUsers[0].dataValues.createdAt
+    // console.log(testDate);
+
+    recentPosts.forEach(post => {
+        dates.push(post.dataValues.createdAt)
+        
+    });
+    console.log("-------");
+    console.log(dates);
+
+    recentPosts.forEach( async (post) => {
+        let id = post.id
+        let userObj = await db.posts.findByPk(id, {
+            include: [{
+                model: db.users,
+                required: true
+            }]})
+        users.push(userObj.dataValues.user.dataValues.username);
+    })
+
+
+    res.render('index', {
+        recentPosts: recentPosts,
+        users: users
+    });
+});
+
+// router.get('/blogs', async (req,res) => {
+//     //put gatekeeper to get user id
+//     let date = new Date()
+//     date.setDate(date.getDate() - 3)
+//     console.log(date);
+
+//     // let recentPosts = await db.posts.findAll(
+//     //     {
+//     //     where: {
+//     //         createdAt: {
+//     //             [Op.gte]: date
+//     //         }
+//     //     },
+//     //      include: {}
+//     // }
+//     // );
+//     let posts = await db.posts.findAll({include: [{
+//         model: db.users,
+//         required: true
+//     }]})
+
+//     let userRecord = await db.users.findByPk(1) //req.user.id 
+//     console.log(userRecord);
+//     // let followingUsers = userRecord.following
+//     res.json(posts)
+// })
 
 //need to figure out if multi posts from one user can go on index or not. might need sep route then render in the /blogs/:id and use ejs for each to build instead, can use blogSkeleton
 // router.get('/blogs/:id', async (req,res) => {
