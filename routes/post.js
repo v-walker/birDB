0fd5a9cc78b66b45acbf21579d2b792fa6072e85
Victoria Walker <vmsmith2014@gmail.com>
2021-11-03 21:29:15 -0400

    wrapped post.js (router) paths in try/catch

--- routes/post.js
+++ routes/post.js
@@ -9,64 +9,98 @@ router.get('/post', (req, res) => {
 
 // GET /post/:postID
 router.get("/post/:postID", async (req, res) => {
-    let postID = req.params.postID;
+    try {
+        let postID = req.params.postID;
 
-    let post = await db.posts.findByPk(postID);
-    let comments = await db.comments.findAll({where: {postID: postID}});
+        let post = await db.posts.findByPk(postID);
+        let comments = await db.comments.findAll({where: {postID: postID}});
 
-    res.json({post, comments});
+        res.json({post, comments});
+    } catch {
+        console.log('Error getting post');
+        res.redirect('/')
+    }
 });
 
 router.put('/post/:postID', async (req, res) => {
-    let postID = req.params.postID;
+    try {
+        let postID = req.params.postID;
 
-    // pulling updated information from edit
-    let {title, observation, imgURL} = req.body;
-    await db.posts.update({title: title, observation: observation, imgURL: imgURL}, {where: {id: postID}});
+        // pulling updated information from edit
+        let {title, observation, imgURL} = req.body;
+        await db.posts.update({title: title, observation: observation, imgURL: imgURL}, {where: {id: postID}});
 
-    let post = await db.posts.findByPk(postID);
-    let comments = await db.comments.findAll({where: {postID: postID}});
+        let post = await db.posts.findByPk(postID);
+        let comments = await db.comments.findAll({where: {postID: postID}});
 
-    res.json({post, comments});
+        res.json({post, comments});
+    } catch {
+        console.log("error while updating post");
+        res.render('post', {
+            error: "Error while updating post."
+        })
+    }
 });
 
 router.delete('/post/:postID', async (req, res) => {
-    let postID = req.params.postID;
+    try {
+        let postID = req.params.postID;
 
-    await db.posts.destroy({where: {id: postID}});
-    // redirect to home/landing page
-    res.redirect('/');
+        await db.posts.destroy({where: {id: postID}});
+        // redirect to home/landing page
+        res.redirect('/');
+    } catch {
+        console.log("error while deleting post");
+        res.render('post', {
+            error: "Error occurred while deleting post"
+        })
+    }
 });
 
 router.post('/post/:postID', async (req, res) => {
-    let postID = req.params.postID;
-    let {username, contents} = req.body;
-    
-    // allow to post new comment to db (associated with a post)
-    await db.comments.create({postID: postID, username: username, contents: contents, likes: '0'});
-    
-    let post = await db.posts.findByPk(postID);
-    let comments = await db.comments.findAll({where: {postID: postID}});
+    try {
+        let postID = req.params.postID;
+        let {username, contents} = req.body;
+        
+        // allow to post new comment to db (associated with a post)
+        await db.comments.create({postID: postID, username: username, contents: contents, likes: '0'});
+        
+        let post = await db.posts.findByPk(postID);
+        let comments = await db.comments.findAll({where: {postID: postID}});
 
-    res.json({post, comments})
+        res.json({post, comments});
+    } catch {
+        console.log("Error while creating new comment");
+        res.render('post', {
+            error: "Error occurred while creating new comment"
+        })
+    }
 });
 
 // editing comment
 router.put('/post/:postID/:commentID', async (req, res) => {
-    let postID = req.params.postID
-    let commentID = req.params.commentID;
-    let updatedContents = req.body.updatedContents;
+    try {
+        let postID = req.params.postID
+        let commentID = req.params.commentID;
+        let updatedContents = req.body.updatedContents;
 
-    await db.comments.update({contents: updatedContents}, {where: {id: commentID}});
+        await db.comments.update({contents: updatedContents}, {where: {id: commentID}});
 
-    let post = await db.posts.findByPk(postID);
-    let comments = await db.comments.findAll({where: {postID: postID}});
+        let post = await db.posts.findByPk(postID);
+        let comments = await db.comments.findAll({where: {postID: postID}});
 
-    res.json({post, comments});
+        res.json({post, comments});
+    } catch {
+        console.log("Error occurred while updating comment");
+        res.render('post', {
+            error: "Error occurred while updating comment"
+        })
+    }
 });
 
 router.delete('/post/:postID/:commentID', async (req, res) => {
-    let postID = req.params.postID;
+    try {
+        let postID = req.params.postID;
     let commentID = req.params.commentID;
 
     await db.comments.destroy({where: {id: commentID}});
@@ -75,6 +109,12 @@ router.delete('/post/:postID/:commentID', async (req, res) => {
     let comments = await db.comments.findAll({where: {postID: postID}});
 
     res.json({post, comments});
+    } catch {
+        console.log("Error occurred while deleting comment");
+        res.render("post", {
+            error: "Error occurred while deleting comment"
+        })
+    }
 });
 
 module.exports = router;
