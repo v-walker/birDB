51f9217211f07e4bce1bd4162e2b0c59aa6feabb
Ryan Donald <ryanthomasdonald@gmail.com>
2021-11-04 15:14:40 -0400

    IMAGE UPLOAD WORKS

--- routes/create.js
+++ routes/create.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router()
-const gatekeeper =  require('../auth');
+const gatekeeper = require('../auth');
 const formidable = require('formidable');
 const cloudinary = require("cloudinary");
-const util = require('util');
+const path = require("path")
+const fs = require("fs")
 require('dotenv').config()
-
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -18,46 +18,25 @@ router.get("/create", (req, res) => {
 })
 
 router.post('/create', (req, res, next) => {
-    
-    const form = formidable({ multiples: true });
-    console.log(form)
+    const form = new formidable.IncomingForm();
+    let uploadFolder = path.join(__dirname, "../public", "files")
+    form.uploadDir = uploadFolder
     form.parse(req, (err, fields, files) => {
         if (err) {
-            next(err);
-            return;
+            next()
+            return
         }
-        cloudinary.uploader.upload(files.upload.path, result => {
-
-            console.log(result)
+        cloudinary.uploader.upload(files.upload.filepath, result => {
+            console.log("this is the result", result)
             if (result.public_id) {
                 res.writeHead(200, { 'content-type': 'text/plain' });
                 res.write('received upload:\n\n');
-                res.end(util.inspect({ fields: fields, files: files }));
+                res.end(JSON.stringify({ fields: fields, files: files }));
             }
-        }
-        );
+        })
+        fs.unlinkSync(files.upload.filepath);
     });
+    
 });
-
-// router.post("/create", (req, res) => {
-//     console.log(req.body)
-
-    // const form = new formidable();
-    // form.parse(req, (err, fields, files) => {
-
-    //     //https://cloudinary.com/documentation/upload_images
-    //     cloudinary.uploader.upload(files.upload.path, result => {
-
-    //         console.log(result)
-    //         if (result.public_id) {
-    //             res.writeHead(200, { 'content-type': 'text/plain' });
-    //             res.write('received upload:\n\n');
-    //             res.end(util.inspect({ fields: fields, files: files }));
-    //         }
-    //     }
-    //     );
-    // });
-    // return;
-// })
 
 module.exports = router
